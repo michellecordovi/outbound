3234c6717e832bb43e16b1e590ec5ba4bb552cf1
Michelle Cordovi <michellecordovi@gmail.com>
2026-03-31 13:48:17 -0400

    add footer component and initial styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css'
-import NavBar from './components/NavBar/NavBar'
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (<>
-    <NavBar/>
-    <h1>Hi, Michelle!</h1>
-    </>
-  )
+	return (
+		<>
+			<NavBar />
+			<h1>Hi, Michelle!</h1>
+			<Footer />
+		</>
+	);
 }
 
-export default App
+export default App;
